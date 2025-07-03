0229362d793e57aa4477a1bd5844cd68be1b0d81
Eula Daniela Yamil <euladanielayamil@gmail.com>
2025-07-03 18:24:49 +0000

    guardo los cambios hasta el momento, fetch iintegrado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,73 @@
-import React from "react";
-
-//include images into your bundle
+import React, { useState, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [newTask, setnewTask] = useState('');
+	const [tasks, setTasks] = useState([]);
+
+
+	useEffect(() => {
+		fetch('https://playground.4geeks.com/todo/users/daniela')
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			setTasks(data);
+		})
+		.catch(error => console.error(error));
+	}, []);
+
+	function addTask(event) {
+		if (event.key === "Enter" && newTask.trim() !== '') {
+			fetch('https://playground.4geeks.com/todo/users/daniela', {
+				method: "POST",
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify ({task : newTask})
+				})
+
+				.then(response => response.json())
+				.then(data => {
+					setTasks([... tasks,data]);
+					setnewTask('');
+				})
+				.catch(error => console.error(error));
+			}
+		}
+	
+
+	function deleteTask(index) {
+		fetch('https://playground.4geeks.com/todo/users/daniela' , {
+			method: 'DELETE'
+		})
+		.then(response => response.json())
+		.then(() => {
+			setTasks(tasks.filter((task, i) => i !== index));
+		})
+		.catch(error => console.error(error));
+
+	}
+
+	return (
+		<div className="container">
+			<div className="text-center">
+				<h3 className="text-center mt-5 text-dark bg-tertiary">To Do List</h3>
+				<div className="card">
+					
+					<input type="text" placeholder="ingresa texto" value={newTask} onChange={(event) => setnewTask(event.target.value)} onKeyDown={addTask}></input>
+
+					<ul>
+						{tasks.map((task, index) => (
+							<li key={index} style={{ listStyleType: 'none' }}>{task}<button className="btn-close" onClick={() => deleteTask(index)}></button></li>
+						))}
+					</ul>
+
+					<p></p>
+				</div>
+			</div>
 		</div>
 	);
 };
